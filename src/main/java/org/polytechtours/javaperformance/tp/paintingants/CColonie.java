61b304c4023e4204b3682bc7b616e7a5b12f16ea
Angèle Roussel <angele.roussel@etu.univ-tours.fr>
2023-01-17 15:50:33 +0100

    refactor Colonie

--- src/main/java/org/polytechtours/javaperformance/tp/paintingants/CColonie.java
+++ src/main/java/org/polytechtours/javaperformance/tp/paintingants/CColonie.java
@@ -9,57 +9,53 @@ package org.polytechtours.javaperformance.tp.paintingants;
  * the Source Creation and Management node. Right-click the template and choose
  * Open. You can then make changes to the template in the Source Editor.
  */
+
 import java.util.Vector;
 
 public class CColonie implements Runnable {
 
-  private Boolean mContinue = Boolean.TRUE;
-  private Vector<CFourmi> mColonie;
-  private PaintingAnts mApplis;
+    private Boolean mContinue = Boolean.TRUE;
+    private final Vector<CFourmi> mColonie;
+    private final PaintingAnts mApplis;
 
-  /**
-   * Creates a new instance of CColonie.
-   * @param pColonie
-   * @param pApplis
-   */
-  public CColonie(Vector<CFourmi> pColonie, PaintingAnts pApplis) {
-    mColonie = pColonie;
-    mApplis = pApplis;
-  }
-
-  /**
-   * Stops the colonization.
-   */
-  public void pleaseStop() {
-    mContinue = false;
-  }
-
-
-  /**
-   * Process of the colonization.
-   */
-  @Override
-  public void run() {
-
-    while (mContinue == true) {
-      if (!mApplis.getPause()) {
-        for (int i = 0; i < mColonie.size(); i++) {
-          mColonie.get(i).deplacer();
-          mApplis.compteur();
-        }
-      } else {
-        /*
-         * try { Thread.sleep(100); } catch (InterruptedException e) { break; }
-         */
-      }
+    /**
+     * Creates a new instance of CColonie.
+     */
+    public CColonie(Vector<CFourmi> pColonie, PaintingAnts pApplis) {
+        mColonie = pColonie;
+        mApplis = pApplis;
     }
-  }
 
-  /**
-   * Checks if the colonization is running or not.
-   * @return True if the colonization continues, else false.
-   */
-  public Boolean getmContinue() {
-    return mContinue;
-  }
+    /**
+     * Stops the colonization.
+     */
+    public void pleaseStop() {
+        mContinue = false;
+    }
+
+
+    /**
+     * Process of the colonization.
+     */
+    @Override
+    public void run() {
+
+        while (mContinue) {
+            if (!mApplis.getPause()) {
+              for (CFourmi cFourmi : mColonie) {
+                cFourmi.deplacer();
+                mApplis.compteur();
+              }
+            }
+        }
+    }
+
+    /**
+     * Checks if the colonization is running or not.
+     *
+     * @return True if the colonization continues, else false.
+     */
+    public Boolean getmContinue() {
+        return mContinue;
+    }
 }
