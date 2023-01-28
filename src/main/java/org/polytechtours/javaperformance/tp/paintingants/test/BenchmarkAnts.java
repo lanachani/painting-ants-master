8fbb1dc67114e912c05a3b67ad13bc83cb98ea98
Angèle Roussel <angele.roussel@etu.univ-tours.fr>
2023-01-28 20:42:57 +0100

    benchmark conforme à ce que on dans le rapport

--- src/main/java/org/polytechtours/javaperformance/tp/paintingants/test/BenchmarkAnts.java
+++ src/main/java/org/polytechtours/javaperformance/tp/paintingants/test/BenchmarkAnts.java
@@ -5,13 +5,21 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.polytechtours.javaperformance.tp.paintingants.CFourmi;
+import org.polytechtours.javaperformance.tp.paintingants.CPainting;
 import org.polytechtours.javaperformance.tp.paintingants.PaintingAnts;
+import org.polytechtours.javaperformance.tp.paintingants.Parameters;
 
+import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
 @State(Scope.Benchmark)
 public class BenchmarkAnts {
-    private final PaintingAnts paint= new PaintingAnts();
+    private final PaintingAnts paint = new PaintingAnts();
+    private final Parameters param = new Parameters();
+    private final CPainting painting = new CPainting(new Dimension(100, 100), paint);
+    private final CFourmi fourmi = new CFourmi(new Color(255, 0, 0), 0.3f, 0.3f, 0.4f,
+            0.5f, painting, 'd', 1, 1, 50, paint);
 
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
@@ -34,6 +42,26 @@ public class BenchmarkAnts {
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
     @BenchmarkMode(Mode.AverageTime)
     public void getParameterInfo() {
-        paint.getParameterInfo();
+        param.getParameterInfo();
     }
+
+    @Benchmark
+    @Fork(value = 1, warmups = 1)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @BenchmarkMode(Mode.AverageTime)
+    public void getDeplacements() {
+        fourmi.getNbDeplacements();
+    }
+
+    @Benchmark
+    @Fork(value = 1, warmups = 1)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @BenchmarkMode(Mode.AverageTime)
+    public void getX() { fourmi.getX(); }
+
+    @Benchmark
+    @Fork(value = 1, warmups = 1)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @BenchmarkMode(Mode.AverageTime)
+    public void getY() { fourmi.getY(); }
 }
